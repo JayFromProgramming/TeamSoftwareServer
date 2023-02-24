f5e1c14720569a014df533411a4c4168ea38e114
JayFromProgramming <ajsweene@mtu.edu>
2023-02-24 13:46:00 -0500

    Initial commit

---
+++ gamemanagers/base_game.py
@@ -0,0 +1,34 @@
+
+
+class BaseGame:
+
+    def __init__(self):
+        self.host = None  # type: User
+        self.users = []  # type: list[User]
+        raise NotImplementedError("BaseGame is an abstract class and cannot be instantiated.")
+
+    def get_game_info(self):
+        example = {
+            "name": "Example Game",
+            "time_elapsed": "",
+            "time_remaining": "",
+            "score": "",
+            "state": "Idle"
+        }
+        raise NotImplementedError
+
+    def user_join(self, user):
+        raise NotImplementedError
+
+    def user_leave(self, user):
+        raise NotImplementedError
+
+    def get_users(self):
+        raise NotImplementedError
+
+    def get_game_state(self, user):
+        raise NotImplementedError
+
+    def post_move(self, user, move):
+        raise NotImplementedError
+
